642fa955ed8cf4232d1b9f7022dd6f9fee2b596c
fabioo-junioor <fj_011@hotmail.com>
2024-08-27 18:59:50 -0300

    feat: create 'formReservation'

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import Navbar from './Navbar.vue';
 
 import FormUser from './forms/FormUser.vue';
+import FormReservation from './forms/FormReservation.vue';
 
 import CardProfessional from './strepper-content/CardProfessional.vue';
 import CalendarSchedule from './strepper-content/CalendarSchedule.vue';
@@ -9,6 +10,7 @@ import TimeSchedule from './strepper-content/TimeSchedule.vue';
 export {
     Navbar,
     FormUser,
+    FormReservation,
     CardProfessional,
     CalendarSchedule,
     TimeSchedule
