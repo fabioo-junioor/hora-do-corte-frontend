c5bdaa18153851b28045a80f3a026e36e5054bbe
fabioo.junioor <fj_011@hotmail.com>
2024-08-27 22:05:56 -0300

    style: settings hover in component 'timeSchedules' and add icon check

--- src/components/strepper-content/TimeSchedule.vue
+++ src/components/strepper-content/TimeSchedule.vue
@@ -1,26 +1,41 @@
 <script setup>
+import { reactive, ref, watch } from 'vue';
+
 const props = defineProps(['time', 'isAvailable']);
 const timeCheck = defineModel('timeCheck');
+const isHovered = ref(false);
 
 </script>
 <template>
   <div id="time-schedules">
     <q-badge
+        :style="'background-color: '+ ((isHovered && props.isAvailable) ? 'grey' : '') +' !important;' +
+          'color: '+ ((isHovered && props.isAvailable) ? 'black' : '') +' !important;' +
+          'cursor: '+ ((isHovered && props.isAvailable) ? 'pointer' : 'not-allowed')"
         class="q-py-xs q-px-sm text-h6"
-        :color="(props.isAvailable && (timeCheck != props.time)) ? 'green-8' : 'grey-5'"
-        :text-color="(props.isAvailable && (timeCheck != props.time)) ? 'white' : 'grey-10'">
+        @mouseover="isHovered = true"
+        @mouseleave="isHovered = false"
+        :color="(props.isAvailable && (timeCheck != props.time)) ? 'green-8' : 'grey-6'"
+        :text-color="(props.isAvailable && (timeCheck != props.time)) ? 'white' : 'black'">
         {{ props.time }}
+        <q-icon
+          v-if="timeCheck == props.time" 
+          class="icon-check" 
+          size="1.1rem" 
+          name="done_outline" 
+          color="white" />
     </q-badge>
   </div>
 </template>
 <style lang="scss" scoped>
 #time-schedules{
   .q-badge{
-    cursor: pointer;
+    position: relative;
 
-    &:hover{
-      background-color: $brown-5 !important;
-
+    .icon-check{
+      position: absolute;
+      right: -5px;
+      top: -5px;
     }
   }
 }
