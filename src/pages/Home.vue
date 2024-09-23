62159cbc1cebe550e0db031c385818451365b15f
fabioo-junioor <fj_011@hotmail.com>
2024-09-23 00:12:12 -0300

    style: added new icons and background wave

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,25 +1,35 @@
 <script setup>
 import { reactive } from 'vue';
+import { useRouter, useRoute } from 'vue-router';
 import { CardPricing } from '../components';
 
+const router = useRouter();
 const pricingPlans = reactive([
     { 
         id: 1, 
         name: 'Prata', 
-        about: 'teste', 
+        about: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.', 
         price: 25, 
         time: 30,
-        benefits: ['30 dias de uso', 'Suporte']
+        benefits: ['30 dias de uso', 'Suporte diário']
     },
     { 
         id: 2, 
         name: 'Ouro', 
-        about: 'teste', 
-        price: 150, 
+        about: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.', 
+        price: 135, 
         time: 180,
-        benefits: ['180 dias de uso', 'Suporte']
+        benefits: ['180 dias de uso', 'Suporte diário']
     }
 ]);
+const buyPlan = (data) => {
+  console.log('buy ', data);
+
+};
+const register = () => {
+  router.push({ path: '/cadUser' });
+
+};
 </script>
 <template>
   <div id="home">
@@ -31,37 +41,63 @@ const pricingPlans = reactive([
       </p>
       
       <q-img 
-        src="../assets/icon/barbearia.svg"
-        height="9rem" width="9rem" 
-        class="home-icon-barbearia" />
-      <q-img 
         src="../assets/icon/navalha.svg"
         height="9rem" width="9rem" 
         class="home-icon-navalha" />
-      <q-img
-        src="../assets/icon/navalha2.svg"
-        height="9rem" width="9rem" 
-        class="home-icon-navalha2" />
       <q-img
         src="../assets/icon/maquina-cortar-cabelo.svg"
         height="9rem" width="9rem" 
         class="home-icon-maquina-cortar-cabelo" />
       
       <q-btn
+        @click="register"
         push
-        class="q-mt-xl"
+        class="q-my-xl"
         size="xl"
         color="brown-8"
         text-color="grey-3"
         label="Resgistre-se gratuitamente"
         icon-right="trending_flat" />
     </div>
-    <div class="home-pricing q-py-xl">
+    <div class="home-works q-py-xl q-my-xl">
+      <h3 class="q-ma-none">Como funciona?</h3>
+      <div class="home-work-profesional">
+        <q-img
+          src="https://www.cbvl.esp.br/upload/post/default.jpg"
+          class="rounded-borders"
+          height="auto"
+          width="50%" />
+        <p class="q-pa-sm">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      </div>
+      <div class="home-work-services">
+        <q-img
+          src="https://www.cbvl.esp.br/upload/post/default.jpg"
+          class="rounded-borders"
+          height="auto"
+          width="50%" />
+        <p class="q-pa-sm">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      </div>
+    </div>
+    <div class="home-pricings q-py-xl q-my-xl">
         <CardPricing
             v-for=" i in pricingPlans" :key="i.id"
-            :dataPricing='i' />
+            class="home-pricing q-my-xl"
+            :dataPricing='i'
+            @buyPlan='buyPlan' />
+        
+        <q-img 
+          src="../assets/icon/navalha2.svg"
+          height="9rem" width="9rem" 
+          class="home-icon-navalha2" />
+        <q-img
+          src="../assets/icon/barbearia.svg"
+          height="9rem" width="9rem" 
+          class="home-icon-barbearia" />
+        <q-img
+          src="../assets/icon/escova-barbear.svg"
+          height="9rem" width="9rem" 
+          class="home-icon-escova-barbear" />
     </div>
-    <div class=""></div>
     <div class=""></div>
   </div>
 </template>
@@ -73,6 +109,10 @@ const pricingPlans = reactive([
   flex-direction: column;
   align-items: center;
   font-family: "Fredoka", sans-serif;
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)),
+      url("../assets/background/background-wave.png") no-repeat
+        fixed center;
+  background-size: cover;
 
   .home-about {
     position: relative;
@@ -81,10 +121,10 @@ const pricingPlans = reactive([
     align-items: center;
     width: 50%;
     color: $whiteColorPrimary;
-    background-color: transparent;
 
     h1{
         font-size: 5rem;
+        font-weight: 500;
         z-index: 2;
 
     }
@@ -99,39 +139,91 @@ const pricingPlans = reactive([
       width: 100%;
 
     }
-    .home-icon-barbearia{
-      position: absolute;
-      bottom: 15%;
-      left: 0%;
-      opacity: .5;
-
-    }
     .home-icon-navalha{
       position: absolute;
       top: 15%;
-      right: 5%;
-      opacity: .5;
-
-    }
-    .home-icon-navalha2{
-      position: absolute;
-      top: 10%;
-      left: 20%;
-      opacity: .5;
+      left: 5%;
+      opacity: .4;
 
     }
     .home-icon-maquina-cortar-cabelo{
       position: absolute;
-      bottom: 0;
+      bottom: 10%;
       right: 10%;
       opacity: .5;
 
     }
   }
-  .home-pricing{
+  .home-works{
     display: flex;
-    gap: 1rem;
+    flex-direction: column;
+    align-items: center;
+    gap: 2rem;
+    width: 80%;
 
+    h3{
+      color: $grey-2;
+      font-weight: 500;
+
+    }
+    .home-work-profesional{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      p{
+        width: 50%;
+        font-size: 1rem;
+        color: $grey-2;
+
+      }
+    }
+    .home-work-services{
+      display: flex;
+      flex-direction: row-reverse;
+      justify-content: center;
+      align-items: center;
+
+      p{
+        width: 50%;
+        font-size: 1rem;
+        color: $grey-2;
+
+      }
+    }
+  }
+  .home-pricings{
+    position: relative;
+    display: flex;
+    justify-content: center;
+    gap: 1rem;
+    width: 80%;
+
+    .home-pricing{
+      z-index: 2;
+
+    }
+    .home-icon-navalha2{
+      position: absolute;
+      top: 10%;
+      left: 10%;
+      opacity: .5;
+
+    }
+    .home-icon-barbearia{
+      position: absolute;
+      bottom: 0;
+      left: 40%;
+      opacity: .5;
+
+    }
+    .home-icon-escova-barbear{
+      position: absolute;
+      bottom: 5%;
+      right: 5%;
+      opacity: .5;
+
+    }
   }
 }
 </style>
