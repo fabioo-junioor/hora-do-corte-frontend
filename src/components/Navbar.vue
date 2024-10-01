3a15320655b71f5bc95789d94dd624779a93f5ff
fabioo.junioor <fj_011@hotmail.com>
2024-09-30 22:13:51 -0300

    Style: changing the position and types of buttons in the navigation bar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -13,8 +13,16 @@ const getLogin = () => {
    router.push({ path: '/loginUser' });
    
 };
+const getLogout = () => {
+   console.log('sair');
+   
+};
 const getEditUser = () => {
    router.push({ path: '/editUser' });
+
+};
+const getEditPassword = () => {
+   console.log('senha');
 
 };
 const getEditProfessional = () => {
@@ -34,40 +42,80 @@ onMounted(() => {
     <q-layout view="hHh lpR fFf">
       <q-header elevated class="q-px-md q-py-xs">
         <q-toolbar>
-          <router-link to="/">
-            <q-toolbar-title>
-              <q-img src="../assets/logo/logo.png" height="4rem" width="4rem" />
-            </q-toolbar-title>
-          </router-link>
-          <div class="btn-area-user"
-            v-if="isUserLogin">
+          <div class="navbar-logo">
+            <router-link to="/">
+              <q-toolbar-title>
+                <q-img src="../assets/logo/logo.png" height="4rem" width="4rem" />
+              </q-toolbar-title>
+            </router-link>
+          </div>
+          <div class="navbar-area-user">
+            <div
+              v-if="isUserLogin" 
+              class="navbar-area-user-login">
               <q-btn 
                   outline
                   @click="getLogin"
                   color="brown-10"
                   icon="login"
                   label="Área do usuário" />
-          </div>
-          <div v-else class="btn-area-user">
-              <q-btn 
+            </div>
+            <div v-else class="navbar-area-user-notLogin row">
+              <div class="col" />
+              <div class="col">
+                <q-btn 
                   outline
                   @click="getReservations"
                   color="brown-10"
                   icon="event_available"
                   label="Agendamentos" />
-              <q-btn 
+              <q-btn-dropdown
                   outline
-                  @click="getEditUser"
                   color="brown-10"
                   icon="settings"
-                  label="Configuração" />
+                  label="Configuração">
+                  <q-list class="bg-brown-5">
+                    <q-item
+                      clickable
+                      v-close-popup
+                      @click="getEditUser">
+                      <q-item-section avatar>
+                        <q-icon name="settings_applications" size="md" color="white"/>
+                      </q-item-section>
+                      <q-item-section>
+                        <q-item-label class="text-white text-h6">Estabelecimento</q-item-label>
+                      </q-item-section>
+                    </q-item>
+                    <q-item
+                      clickable
+                      v-close-popup
+                      @click="getEditPassword">
+                      <q-item-section avatar>
+                        <q-icon name="password" size="md" color="white"/>
+                      </q-item-section>
+                      <q-item-section>
+                        <q-item-label class="text-white text-h6">Alterar senha</q-item-label>
+                      </q-item-section>
+                    </q-item>
+                  </q-list>  
+                </q-btn-dropdown>
               <q-btn 
                   outline
                   @click="getEditProfessional"
                   color="brown-10"
                   icon="manage_accounts"
                   label="Profissionais" />
-          </div>
+              </div>
+              <div class="col">
+                <q-btn 
+                  outline
+                  @click="getLogout"
+                  color="brown-10"
+                  icon="logout"
+                  label="Sair" />
+              </div>
+            </div>
+          </div>          
         </q-toolbar>
       </q-header>
 
@@ -88,7 +136,58 @@ onMounted(() => {
       .q-toolbar{
         display: flex;
         justify-content: space-between;
-        
+        align-items: center;
+
+        .navbar-logo{
+          height: 100%;
+          width: 4.5rem;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+
+        }
+        .navbar-area-user{
+          height: 100%;
+          display: flex;
+          width: calc(100% - 5rem);
+
+          .navbar-area-user-login{
+            width: 100%;
+            display: flex;
+            justify-content: flex-end;
+
+          }
+          .navbar-area-user-notLogin{
+            width: 100%;
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+
+            div{
+              height: 100%;
+
+              .q-btn{
+                height: 100%;
+
+              }
+            }
+          }
+          .navbar-area-user-notLogin div:nth-child(2){
+           display: flex;
+           gap: .5rem;
+
+          }
+          .navbar-area-user-notLogin div:nth-child(3){
+           display: flex;
+           justify-content: flex-end;      
+
+          }
+          .q-btn:hover{
+            background-color: $brown-5 !important;
+
+          }
+        }        
         a{
           text-decoration: none;
           border: 2px solid $brown-5;
@@ -100,17 +199,6 @@ onMounted(() => {
           }
         }
       }
-        .btn-area-user{
-          height: 4rem;
-          display: flex;
-          gap: .5rem;
-          
-          .q-btn:hover{
-            background-color: $brown-10 !important;
-            color: $whiteColorPrimary !important;
-
-          }
-        }
     }
 }
 </style>
