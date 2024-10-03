28293c19a92724012d497ea183394b2ae6c6f38c
fabioo-junioor <fj_011@hotmail.com>
2024-10-02 21:17:48 -0300

    refator: new style 'login' and 'cad' user

--- src/pages/users/CadUser.vue
+++ src/pages/users/CadUser.vue
@@ -15,21 +15,21 @@ const createUser = () => {
 </script>
 <template>
     <div id="cad-user">
-        <div class="cad-user">
-            <div class="cad-user-img">
-                <q-img 
-                    src="../../assets/draw/undraw_hello.svg"
-                    fit="contain" />
-            </div>
-            <div class="cad-user-forms">
-                <FormUser
-                    class="form-user"
-                    typeForm="cadUser"
-                    v-model:email="dataFormUser.email"
-                    v-model:password="dataFormUser.password"
-                    v-model:repeatPassword="dataFormUser.repeatPassword"
-                    @createUser="createUser" />
-            </div>
+        <div class="cad-user-img">
+            <q-img 
+                src="../../assets/logo/logo.png"
+                height="80%"
+                width="80%"
+                fit="contain" />
+        </div>
+        <div class="cad-user-forms q-px-sm">
+            <FormUser
+                class="form-user"
+                typeForm="cadUser"
+                v-model:email="dataFormUser.email"
+                v-model:password="dataFormUser.password"
+                v-model:repeatPassword="dataFormUser.repeatPassword"
+                @createUser="createUser" />
         </div>
     </div>
 </template>
@@ -38,52 +38,36 @@ const createUser = () => {
 
 #cad-user{
     display: flex;
-    flex-direction: column;
     justify-content: center;
     align-items: center;
     height: calc(100vh - 5rem);
     font-family: "Fredoka", sans-serif;
 
-    .cad-user{
+    .cad-user-img{
+        width: 50%;
+        height: 100%;
         display: flex;
         justify-content: center;
         align-items: center;
         background-color: $darkColorSecondary;
-        box-shadow: 0px 0px 10px 3px rgba(0, 0, 0, .3);
-        border: 1px solid $brown-10;
-        border-radius: 5px;
-        width: 60%;
-        height: 80%;
 
-        .cad-user-img{
-            width: 50%;
+    }
+    .cad-user-forms{
+        display: flex;
+        justify-content: center;
+        width: 50%;
+        height: 100%;
+
+        .form-user{
             height: 100%;
-            border-radius: 5px;
-            background-color: $darkColorSecondary;
-
-            .q-img{
-                height: 100%;
+            width: 100%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+    
+            .q-form{
                 width: 100%;
                 
-            }
-        }
-        .cad-user-forms{
-            display: flex;
-            justify-content: center;
-            width: 50%;
-            height: 100%;
-
-            .form-user{
-                height: 100%;
-                width: 100%;
-                display: flex;
-                align-items: center;
-                justify-content: center;
-        
-                .q-form{
-                    width: 100%;
-                    
-                }
             }
         }
     }
