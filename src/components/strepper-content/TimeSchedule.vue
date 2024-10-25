2fe271c202eb1cffc683f273dce7f07245db4086
fabioo.junioor <fj_011@hotmail.com>
2024-10-25 00:13:39 -0300

    style: applay hovered in component times

--- src/components/strepper-content/TimeSchedule.vue
+++ src/components/strepper-content/TimeSchedule.vue
@@ -7,15 +7,15 @@ const isHovered = ref(false);
 
 const verifyReservationColorBg = (data, time) =>{
   if(data){
-    return 'red';
+    return 'red-8';
 
   }
   if(!data && time == timeCheck.value){
-    return 'grey';
+    return 'grey-8';
 
   };
   if(!data && time != timeCheck.value){
-    return 'green';
+    return 'green-8';
 
   }
 };
@@ -27,15 +27,15 @@ const verifyReservationColorBg = (data, time) =>{
         class="q-py-xs q-px-sm text-h6"
         @mouseover="isHovered = true"
         @mouseleave="isHovered = false"
-        :color="verifyReservationColorBg(props.isReserved, props.time)"
-        text-color="white">
+        :color="(isHovered && !props.isReserved) ? 'grey-2' : verifyReservationColorBg(props.isReserved, props.time)"
+        :text-color="(isHovered && !props.isReserved) ? 'grey-8' : 'grey-2'">
         {{ props.time }}
         <q-icon
           v-if="(timeCheck == props.time && !props.isReserved)" 
           class="icon-check" 
           size="1.1rem" 
           name="done_outline" 
-          color="white" />
+          color="green-5" />
     </q-badge>
   </div>
 </template>
